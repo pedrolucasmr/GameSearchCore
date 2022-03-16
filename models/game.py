ce9a6dfe0dff01fbfc95e34d8dffb82e5f9d7b30
Pedro Lucas Maia Ramos <pl.maia@hotmail.com>
2022-03-16 10:03:43 -0300

    Backup

--- models/game.py
+++ models/game.py
@@ -1,5 +1,18 @@
 class Game:
-    def __init__(self, age_ratings, aggregated_reviews):
+    def __init__(self, age_ratings, aggregated_reviews, category, release_date, genres, companies, name, platforms, rating, url):
         self.age_ratings = age_ratings
         self.aggragated_reviews = aggregated_reviews
+        self.category = category
+        self.release_date = release_date
+        self.genres = genres
+        self.companies = companies
+        self.name = name
+        self.platforms = platforms
+        self.rating = rating
+        self.url = url
+
+
+
+
+
 
